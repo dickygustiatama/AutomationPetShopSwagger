a54983a0fc1792e835c9f3e0c2990418f2481891
Mukhlis Anshori Witanto <mukhlisanshori1997@gmail.com>
2023-01-19 13:37:15 +0700

    templating

---
+++ src/test/java/starter/swagger/PetAPI.java
@@ -0,0 +1,25 @@
+package starter.swagger;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+import starter.swagger.Utils.Constant;
+
+import java.io.File;
+
+public class PetAPI {
+    //Pet
+    public static String POST_IMAGE_PET = Constant.BASE_URL+"/pet/{petId}/uploadImage" ;
+    public static String POST_NEW_PET = Constant.BASE_URL +"/pet" ;
+    public static String PUT_UPDATE_PET = Constant.BASE_URL+"/pet";
+    public static String GET_PET_STATUS = Constant.BASE_URL+"/pet/findByStatus";
+    public static String GET_PET_ID = Constant.BASE_URL+"/pet/{petId}";
+    public static String POST_UPDATE_PET = Constant.BASE_URL+"/pet/{petId}" ;
+    public static String DELETE_PET_ID = Constant.BASE_URL+"/pet/{petId}" ;
+
+    @Step("Post new pet")
+    public void postNewPet(File json){
+        SerenityRest.given().contentType(ContentType.JSON).body(json);
+    }
+
+}
